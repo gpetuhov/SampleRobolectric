68192435408c7f0605a8e186eec79c05c47acedd
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-10-05 17:25:21 +0300

    Change text on click

--- app/src/main/java/com/gpetuhov/android/samplerobolectric/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplerobolectric/MainActivity.kt
@@ -2,11 +2,14 @@ package com.gpetuhov.android.samplerobolectric
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.setOnClickListener { textView.text = getString(R.string.clicked) }
     }
 }
